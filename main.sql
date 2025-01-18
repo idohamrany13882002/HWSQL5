88038be4d3fa94325c46f061da4a2b0911bf2377
idohamrany <idohamrany13882002@gmail.com>
2025-01-18 15:17:43 +0200

    First upload

---
+++ main.sql
@@ -0,0 +1,19 @@
+---exc.1---
+"""
+like group by but the action is row based and not group based
+"""
+
+---exc.2---
+"""
+access data from a JSON form
+"""
+
+---exc.3---
+"""
+int [] lets you set a array of integer values
+"""
+
+---exc.4---
+"""
+Generating values base of a fixed formula
+"""
